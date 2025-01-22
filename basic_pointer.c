25663fa7aca89d9a8f8a56a9ca272d012635e649
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-21 16:04:30 -0800

    Added code for basic pointer

--- basic_pointer.c
+++ basic_pointer.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv){
     int *numPtr;
     int num2;
     num = 100;
-    numPtr = &num;
+    numPtr = &num; //address of operator
     num2 = *numPtr;
     printf("num=%d, numPtr=%p, address of num=%d,num2=%d\n",num,numPtr,num,num2);
     return 0;
