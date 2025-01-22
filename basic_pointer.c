ae2afe14df5b335a246169898d427b4e5e327458
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-21 16:02:41 -0800

    Added gitignore file

---
+++ basic_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int num;
+    int *numPtr;
+    int num2;
+    num = 100;
+    numPtr = &num;
+    num2 = *numPtr;
+    printf("num=%d, numPtr=%p, address of num=%d,num2=%d\n",num,numPtr,num,num2);
+    return 0;
+}
